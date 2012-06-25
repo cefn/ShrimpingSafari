a8bd33500d3ea736ed069534855a5b5723813bde
Laptop Shrimping <laptop@shrimped-compaq-presario700.(none)>
2012-06-25 02:26:22 +0100

    Fixed delay arising from unnecessary buffering.

--- Laptop/eq_sender.py
+++ Laptop/eq_sender.py
@@ -21,12 +21,12 @@ class Eq:
 		self.unitCols = 8 # number of columns of RGB units (units per strip)
 		self.unitRows = 8 # number of rows of RGB units (number of strips)
 		self.lowerDb = -70 # the volume corresponding with MIN (zero) on eq display
-		self.upperDb = -20 # the volume corresponding with MAX (unitCols) on eq display
+		self.upperDb = -35 # the volume corresponding with MAX (unitCols) on eq display
 
 		# connect to the primary soundcard to do fast fourier analysis of frequencies
 		# other sources are available, this samples every 0.04secs (written in nanoseconds)
-		# and assigns frequencies to 16 bands, only the first 8 of which are shown
-		listener_desc = 'pulsesrc ! spectrum interval=20000000 bands=16 threshold=-70 ! fakesink'
+		# and assigns frequencies to 16 bands, (only the first 8 of which are later sent onto the display)
+		listener_desc = 'pulsesrc ! spectrum interval=40000000 bands=16 threshold=-70 ! fakesink'
 		self.listener = gst.parse_launch(listener_desc)
 		bus = self.listener.get_bus()
 		bus.add_signal_watch()
@@ -36,18 +36,18 @@ class Eq:
 	def on_message (self, bus, message):
 		s = message.structure
 		if s and s.get_name() == "spectrum":
-			bandMagnitudes = s['magnitude']
-			# throw away other frequency bands
-			offset = 2
-			selectedBandMagnitudes = bandMagnitudes[offset:self.unitCols + offset] 
+			offset=4
+			#keep only some of the 16 bands, (from 4 to 12)
+			bandMagnitudes = s['magnitude'][ offset : self.unitCols+offset ]
 			# traverse the columns constructing the serial message for the display
 			magnitudeBytes = bytearray([])
 			for unitCol in range(0,self.unitCols):
-				bandMagnitude = selectedBandMagnitudes[unitCol]
+				bandMagnitude = bandMagnitudes[unitCol]
 				bandRow = interp(bandMagnitude,[self.lowerDb,self.upperDb],[0,self.unitRows], 0, self.unitRows) # remap amplitude into range 0-7
 				bandRow = min(self.unitRows,max(0,int(math.floor(bandRow))))
 				magnitudeBytes.extend([bandRow])
 			# write the new values to the display
+			self.usbSerial.flushOutput()			
 			self.usbSerial.write(magnitudeBytes + bytearray('\n'))
 		return True
  
